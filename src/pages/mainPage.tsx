945d2634cb9c4ff64b930aaffc11d402d8939fb9
Artem <fdggfh@gmail.com>
2024-09-18 10:32:26 +0300

    loader

--- src/pages/mainPage.tsx
+++ src/pages/mainPage.tsx
@@ -5,6 +5,8 @@ import { Pagination } from 'swiper/modules';
 import Main from './main/main';
 import Competitions from './competitions/competitions';
 import { useGetCompetitionsQuery } from '../shared/api';
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
 
 export default function MainPage() {
    const { data: competitions, isFetching: isCompetitionsFetching } = useGetCompetitionsQuery({});
@@ -22,7 +24,11 @@ export default function MainPage() {
    return (
       <div className="h-screen">
          {isCompetitionsFetching ? (
+         <Box sx={{ display: 'flex' }}>
             <div className="text-white text-center">Loading...</div>
+            <CircularProgress />
+          </Box>
+            
          ) : (
             <Swiper
                slidesPerView={1}
@@ -31,15 +37,15 @@ export default function MainPage() {
                autoHeight={true}
                className="mySwiper w-full h-full"
             >
-               <SwiperSlide className="flex flex-col items-center justify-center text-xl bg-slate-600 h-full p-4">
+               <SwiperSlide className="flex flex-col items-center justify-center text-x h-full p-4">
                   <Main />
                </SwiperSlide>
 
-               <SwiperSlide className="flex items-center justify-center text-xl bg-slate-600 h-full p-4">
+               <SwiperSlide className="flex items-center justify-center text-xl h-full p-4">
                   <Competitions competitions={competitions}/>
                </SwiperSlide>
 
-               <SwiperSlide className="flex items-center justify-center text-xl bg-slate-600 h-full p-4">
+               <SwiperSlide className="flex items-center justify-center text-xl h-full p-4">
                   Друзья
                </SwiperSlide>
             </Swiper>
